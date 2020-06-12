7a3b34973aeb10d2bc6bad3a015df90ddb3b81a0
Anna <anna.kuhir@nure.ua>
2020-06-12 17:55:09 +0300

    added logo component and get logo title from api

---
+++ src/logo.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { getApiData } from './service';
+
+class Logotype extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      logo: ''
+    };
+  }
+
+  componentDidMount() {
+    getApiData().then(data => {
+      this.setState({logo: data.meta.title})
+    });
+  }
+
+  render() {
+    return <h1>{this.state.logo}</h1>;
+  }
+}
+
+export default Logotype;
---
+++ src/service.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function getApiData() {
+  return fetch('https://us-central1-cms-edu-2020-api.cloudfunctions.net/app/api/v1/section/navigation')
+    .then((data) => data.json())
+    .then((res) => res)
+}
